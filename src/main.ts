d26c86fd1b51c7f02083085fb8fcea60b654b8e3
Maxim <benyachef@gmail.com>
2023-07-20 21:14:38 +0300

    vercel trabl

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import cookieParser from 'cookie-parser';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  await app.listen(3003);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors()
+    app.use(cookieParser())
+    app.useGlobalFilters()
+    app.useGlobalPipes(new ValidationPipe())
+    await app.listen(3003);
 }
 bootstrap();
