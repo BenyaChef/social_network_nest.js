29fa04e66ea4f58920d79917c97070041a666c62
Maxim <benyachef@gmail.com>
2023-09-28 13:07:21 +0300

    quiz game create

--- src/app.module.ts
+++ src/app.module.ts
@@ -143,14 +143,20 @@ const controllers = [
 
 const options: TypeOrmModuleOptions  = {
   type: 'postgres',
-  url: process.env.POSTGRESQL_HOST || 'postgresql://postgres:sa@localhost:5432/social_network',
+  url: process.env.POSTGRESQL_HOST || 'postgres://BenyaChef:uz9LBTdVMDW5@ep-lucky-sun-90762571.eu-central-1.aws.neon.tech/neondb',
   // host: process.env.POSTGRESQL_HOST,
   // port: 5432,
   // username: process.env.POSTGRESQL_NAME || 'postgres',
   // password: process.env.POSTGRESQL_PASS || 'sa',
   // database: process.env.POSTGRESQL_DB || 'social_network',
   autoLoadEntities: true,
-  synchronize: true
+  synchronize: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+      sslmode: "require"
+    }
+  }
 }
 
 const validators = [
